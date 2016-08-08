fa25f7e1cbce01f3dc552186faedb339f8a8d636
Chris M Snyder <iplayguitar81@yahoo.com>
2016-08-08 16:58:24 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/boxscores/index.blade.php
+++ resources/views/boxscores/index.blade.php
@@ -31,7 +31,9 @@
             {{-- */$x++;/* --}}
             <tr>
                 {{--<td>{{ $x }}</td>--}}
-                <td><a href="{{ route('boxscores.show', [$item->id, str_slug($item->title)]) }}">{{ $item->title }}</a> <span class="pull-left"><br/>{{'Post images: '. $item->images()->count() }}</span></td><td>{{ $item->subHead }}</td><td>{{ $item->created_at->format('M dS Y') }}</td><td>
+                <td><a href="{{ route('boxscores.show', [$item->id, str_slug($item->title)]) }}">{{ $item->title }}</a> <span class="pull-left"><br/>
+                        {{--{{'Post images: '. $item->images()->count() }}--}}
+                    </span></td><td>{{ $item->subHead }}</td><td>{{ $item->created_at->format('M dS Y') }}</td><td>
 
                     {{strip_tags(str_limit($item->body, 20))}}
 
