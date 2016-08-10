6a18bca39e1816e772c4bd072cf44b784dbb5688
Chris M Snyder <iplayguitar81@yahoo.com>
2016-08-09 18:02:21 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/boxscores/show.blade.php
+++ resources/views/boxscores/show.blade.php
@@ -54,6 +54,7 @@
 
         {{--This is where I need to add Starter stuff and DRY logic.....--}}
 
+        {{-- */ $start_class1,$start_class2,$start_class3,$start_class4,$start_class5,$start_class6,$start_class7,$start_class8,$start_class9,$start_class10,$start_class11,$start_class12,$start_class13  /* --}}
 
         {{-- */ $away_total_to =$boxscore->ap1_to.$boxscore->ap2_to.$boxscore->ap3_to.$boxscore->ap4_to.$boxscore->ap5_to.$boxscore->ap6_to.$boxscore->ap7_to.$boxscore->ap8_to.$boxscore->ap9_to.$boxscore->ap10_to.$boxscore->ap11_to.$boxscore->ap12_to.$boxscore->ap13_to /* --}}
         {{-- */ $home_total_to =$boxscore->hp1_to.$boxscore->hp2_to.$boxscore->hp3_to.$boxscore->hp4_to.$boxscore->hp5_to.$boxscore->hp6_to.$boxscore->hp7_to.$boxscore->hp8_to.$boxscore->hp9_to.$boxscore->hp10_to.$boxscore->hp11_to.$boxscore->hp12_to.$boxscore->hp13_to /* --}}
