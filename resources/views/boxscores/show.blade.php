69abead2340b33479b487ae7b9fc10a672d9398d
Chris M Snyder <iplayguitar81@yahoo.com>
2016-08-09 18:25:26 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/boxscores/show.blade.php
+++ resources/views/boxscores/show.blade.php
@@ -54,11 +54,6 @@
 
         {{--This is where I need to add Starter stuff and DRY logic.....--}}
 
-        {{-- */ $start_class1=""; $start_class2=""; $start_class3=""; $start_class4=""; $start_class5=""; $start_class6=""; $start_class7=""; $start_class8=""; $start_class9=""; $start_class10=""; $start_class11=""; $start_class12=""; $start_class13="";  /* --}}
-        {{-- */ $astart_class1=""; $astart_class2=""; $astart_class3=""; $astart_class4=""; $astart_class5=""; $astart_class6=""; $astart_class7=""; $astart_class8=""; $astart_class9=""; $astart_class10=""; $astart_class11=""; $astart_class12=""; $astart_class13="";  /* --}}
-        
-        {{-- */$a_starter0="";  $a_starter1=""; $a_starter2=""; $a_starter3=""; $a_starter4=""; $a_starter5=""; $a_starter6=""; $a_starter7=""; $a_starter8=""; $a_starter9=""; $a_starter10=""; $a_starter11=""; $a_starter12=""; $a_starter13="";  /* --}}
-
 
         <div class='box_score'><h2 class='story_h2'><span class='article_title'>{!! $home_or_away !!}</span><br/><span class='box_arena_h2'>{{$boxscore->arena}} | </span><span class='box_start_date_h2'>Insert Date</span><br/>{!! $win_or_loss !!}</h2></div>
 
