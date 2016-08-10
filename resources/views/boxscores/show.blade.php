63143301271d123356711f9670802419759c250a
Chris M Snyder <iplayguitar81@yahoo.com>
2016-08-09 18:24:31 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/boxscores/show.blade.php
+++ resources/views/boxscores/show.blade.php
@@ -59,8 +59,6 @@
         
         {{-- */$a_starter0="";  $a_starter1=""; $a_starter2=""; $a_starter3=""; $a_starter4=""; $a_starter5=""; $a_starter6=""; $a_starter7=""; $a_starter8=""; $a_starter9=""; $a_starter10=""; $a_starter11=""; $a_starter12=""; $a_starter13="";  /* --}}
 
-        {{-- */ $away_total_to =$boxscore->ap1_to.$boxscore->ap2_to.$boxscore->ap3_to.$boxscore->ap4_to.$boxscore->ap5_to.$boxscore->ap6_to.$boxscore->ap7_to.$boxscore->ap8_to.$boxscore->ap9_to.$boxscore->ap10_to.$boxscore->ap11_to.$boxscore->ap12_to.$boxscore->ap13_to /* --}}
-        {{-- */ $home_total_to =$boxscore->hp1_to.$boxscore->hp2_to.$boxscore->hp3_to.$boxscore->hp4_to.$boxscore->hp5_to.$boxscore->hp6_to.$boxscore->hp7_to.$boxscore->hp8_to.$boxscore->hp9_to.$boxscore->hp10_to.$boxscore->hp11_to.$boxscore->hp12_to.$boxscore->hp13_to /* --}}
 
         <div class='box_score'><h2 class='story_h2'><span class='article_title'>{!! $home_or_away !!}</span><br/><span class='box_arena_h2'>{{$boxscore->arena}} | </span><span class='box_start_date_h2'>Insert Date</span><br/>{!! $win_or_loss !!}</h2></div>
 
