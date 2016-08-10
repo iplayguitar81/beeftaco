a1b6dc2f5acebc00cabad9169d7edb766ee4944a
Chris M Snyder <iplayguitar81@yahoo.com>
2016-08-09 17:25:31 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/boxscores/show.blade.php
+++ resources/views/boxscores/show.blade.php
@@ -14,7 +14,7 @@
     <div class="col-md-12">
 
     <article class="center-block">
-        <h1 class="article-title-show" style="font-family: Pacifico, cursive;font-size:4em;line-height:1.2em;text-align:center;">{{ $boxscore->game_string }}</h1>
+        {{--<h1 class="article-title-show" style="font-family: Pacifico, cursive;font-size:4em;line-height:1.2em;text-align:center;"></h1>--}}
 
         {{-- */ $game_date = strtotime($boxscore->datey); $game_date=date("F d Y", $game_date); /* --}}
         <p class="subheader-main" style="text-align:center;font-family: Boogaloo, cursive; font-size:3em;">{{ $game_date}}</p>
