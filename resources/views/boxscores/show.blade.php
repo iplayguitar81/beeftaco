13234f0791ed43bc7f8ac581241baa8872e949d5
Chris M Snyder <iplayguitar81@yahoo.com>
2016-08-10 15:19:46 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/boxscores/show.blade.php
+++ resources/views/boxscores/show.blade.php
@@ -23,7 +23,7 @@
         @php
         $game_date = new DateTime($boxscore->datey, new DateTimeZone('America/Los_Angeles'));
         $game_date = date_sub($game_date, date_interval_create_from_date_string('3 hour'));
-        $game_date = $game_date->format('w F dS Y g:i:s a');
+        $game_date = $game_date->format('l F dS Y g:i:s a');
 
 
 
