13d42661d0f1592999d19c38bd2a1c573eef4712
Chris M Snyder <iplayguitar81@yahoo.com>
2016-08-10 15:25:05 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/boxscores/show.blade.php
+++ resources/views/boxscores/show.blade.php
@@ -29,7 +29,7 @@
 
         @endphp
 
-        <p class="subheader-main" style="text-align:center;font-family: Boogaloo, cursive; font-size:3em;">{{ $game_date}}</p>
+
 
         @if($boxscore->h_initials =='POR')
             {{-- */ $home_or_away = $boxscore->afname."<span class='box_total_h2'> ".$boxscore->atotal."</span><br/>at<br/> portland trail blazers <span class='box_total_h2'>".$boxscore->htotal."</span>" /* --}}
