d8bf17d5a19fa82dd4ae7f62e3e711d1c262e8d2
Chris M Snyder <iplayguitar81@yahoo.com>
2016-08-12 13:26:50 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -993,14 +993,13 @@ h2.story_h2 {
     ga('send', 'pageview');
 
 </script>
-{{--<div id="fb-root"></div>--}}
-{{--<script>(function(d, s, id) {--}}
-        {{--var js, fjs = d.getElementsByTagName(s)[0];--}}
-        {{--if (d.getElementById(id)) return;--}}
-        {{--js = d.createElement(s); js.id = id;--}}
-        {{--js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6&appId=590306801102510";--}}
-        {{--fjs.parentNode.insertBefore(js, fjs);--}}
-    {{--}(document, 'script', 'facebook-jssdk'));</script>--}}
+<script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=590306801102510";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
 
 
 <nav class="navbar navbar-default navbar-fixed-top">
