3ddcf5c6b364838f34d768af250dc01efb331654
Chris M Snyder <iplayguitar81@yahoo.com>
2016-08-14 15:46:16 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,27 +14,27 @@
 
     {{--{{'My sources tell me your location is '.$location['city'].','.$location['state']}}--}}
 
-        <div class="item">
-            <ul id="content-slider" class="content-slider">
-                <li>
-                    <h3>1</h3>
-                </li>
-                <li>
-                    <h3>2</h3>
-                </li>
-                <li>
-                    <h3>3</h3>
-                </li>
-                <li>
-                    <h3>4</h3>
-                </li>
-                <li>
-                    <h3>5</h3>
-                </li>
-                <li>
-                    <h3>6</h3>
-                </li>
-            </ul>
+        <div id="owl-demo" class="owl-carousel owl-theme">
+            <div class="item"><h1>1</h1></div>
+            <div class="item"><h1>2</h1></div>
+            <div class="item"><h1>3</h1></div>
+            <div class="item"><h1>4</h1></div>
+            <div class="item"><h1>5</h1></div>
+            <div class="item"><h1>6</h1></div>
+            <div class="item"><h1>7</h1></div>
+            <div class="item"><h1>8</h1></div>
+            <div class="item"><h1>9</h1></div>
+            <div class="item"><h1>10</h1></div>
+            <div class="item"><h1>11</h1></div>
+            <div class="item"><h1>12</h1></div>
+            <div class="item"><h1>13</h1></div>
+            <div class="item"><h1>14</h1></div>
+            <div class="item"><h1>15</h1></div>
+            <div class="item"><h1>16</h1></div>
+            <div class="item"><h1>17</h1></div>
+            <div class="item"><h1>18</h1></div>
+            <div class="item"><h1>19</h1></div>
+            <div class="item"><h1>20</h1></div>
         </div>
 
 
