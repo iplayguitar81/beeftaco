e4b50ea007071f3d1bfaa135aa2bf8314f5eb21b
Chris M Snyder <iplayguitar81@yahoo.com>
2016-08-14 15:56:10 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,21 +20,6 @@
             <div class="item"><h1>3</h1></div>
             <div class="item"><h1>4</h1></div>
             <div class="item"><h1>5</h1></div>
-            <div class="item"><h1>6</h1></div>
-            <div class="item"><h1>7</h1></div>
-            <div class="item"><h1>8</h1></div>
-            <div class="item"><h1>9</h1></div>
-            <div class="item"><h1>10</h1></div>
-            <div class="item"><h1>11</h1></div>
-            <div class="item"><h1>12</h1></div>
-            <div class="item"><h1>13</h1></div>
-            <div class="item"><h1>14</h1></div>
-            <div class="item"><h1>15</h1></div>
-            <div class="item"><h1>16</h1></div>
-            <div class="item"><h1>17</h1></div>
-            <div class="item"><h1>18</h1></div>
-            <div class="item"><h1>19</h1></div>
-            <div class="item"><h1>20</h1></div>
         </div>
 
 
