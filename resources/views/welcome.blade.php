eeb070627df7c63314a0d06391ed2093e2b455a1
Chris M Snyder <iplayguitar81@yahoo.com>
2016-08-14 15:37:38 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -188,5 +188,5 @@
 </script>
 
 
-<script src="{{url('http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js')}}"></script>
+<script src="{{url('https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js')}}"></script>
 <script src="{{url('/js/lightslider.js')}}"></script>
