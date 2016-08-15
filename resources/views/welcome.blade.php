f2c77c60341017b00c34994f762184354bd554c6
Chris M Snyder <iplayguitar81@yahoo.com>
2016-08-14 18:42:54 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -104,7 +104,7 @@
                         <tr><th colspan="3">{{$game_time}}</th></tr>
                         <tr><td>{{$item->a_initials}}<br/><span class='slider_score'>{{$item->.atotal}}</span></td><td>{!! $versus_or_at !!}<br/><br/>{!! win_or_loss!!}</td><td>{{$item->h_initials}}<br/><span class='slider_score'>{{$item->htotal}}</span></td></tr>
                         <tr><td colspan="3"><div data-uk-button-radio><a href="{{ route('boxscores.show', [$item->id, str_slug($item->game_string)]) }}">Boxscore</a>  </div></td></tr>
-
+                        </table>
 
 
 			</div>
