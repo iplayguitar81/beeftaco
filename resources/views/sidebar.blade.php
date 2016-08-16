1d1159bf5e6e465e3136c0c8786f04ba03fed3ab
Chris M Snyder <iplayguitar81@yahoo.com>
2016-08-15 21:43:27 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -1,4 +1,4 @@
-<div class="panel panel-success"> <div class="panel-heading"> <h3 class="panel-title">Welcome to Trail Blazers Fans.com</h3> </div> <div class="panel-body"> Panel content </div> </div>
+<div class="panel panel-danger"> <div class="panel-heading"> <h3 class="panel-title">Welcome to Trail Blazers Fans.com</h3> </div> <div class="panel-body"> Panel content </div> </div>
 
 <br>
 <ul class="nav nav-tabs">
