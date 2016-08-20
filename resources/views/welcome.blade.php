a659bc4a80b9e7abbef74b50da07cd7326c89fb8
Chris M Snyder <iplayguitar81@yahoo.com>
2016-08-19 18:05:30 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -112,18 +112,11 @@
 
         <div class="col-md-12 blogShort">
             <div class="col-md-12">
-
-
-            <h1>Title 1</h1>
+            <h1 class="secondary-posts-title">Title 1</h1>
             </div>
-
             <div class="col-md-3">
-
                 <img style="width:100%;height:130px;margin-right: 1em;" src="" alt="post img" class="pull-left img-responsive thumb margin10 img-thumbnail">
-
             </div>
-
-
             <div class="col-md-9">
             <em>Written By: <a href="#">Author Name </a></em>
             <article><p>
