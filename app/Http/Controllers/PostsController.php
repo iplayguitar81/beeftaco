ddacacc71db5ba7ac8906face3de31f7df9280e3
Chris M Snyder <iplayguitar81@yahoo.com>
2016-08-21 23:55:44 -0700

    get win 10 laravel started yeahhhhhh

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -263,7 +263,7 @@ class PostsController extends Controller
     {
         $post = Post::findOrFail($id);
         $checked = $post->main_article;
-        $checked = ($checked ? 'false' : 'true');
+        $checked = ($checked ? 'true' : 'false');
 
         return view('posts.edit', compact('post','checked'));
         $this->authorize('isAdmin');
