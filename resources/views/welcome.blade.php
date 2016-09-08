f1321a219ad0d4f0be056225594730f7f68115db
Chris M Snyder <iplayguitar81@yahoo.com>
2016-09-07 22:53:26 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -123,37 +123,13 @@
 
 
         <h2 id="latest_games" class="Ripper">more news</h2>
-        <div class="col-md-7 col-lg-7">
-            <ul class="media-list main-list">
-                <li class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/150x90" alt="...">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">Lorem ipsum dolor asit amet</h4>
-                        <p class="by-author">By Jhon Doe</p>
-                    </div>
-                </li>
-                <li class="media col-md-12">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/150x90" alt="...">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">Lorem ipsum dolor asit amet</h4>
-                        <p class="by-author">By Jhon Doe</p>
-                    </div>
-                </li>
-                <li class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/150x90" alt="...">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">Lorem ipsum dolor asit amet</h4>
-                        <p class="by-author">By Jhon Doe</p>
-                    </div>
-                </li>
-            </ul>
-        </div>
+
+
+
+
+        <hr>
+
+        <br/>
 
         <h2 id="latest_games" class="Ripper">more news</h2>
 
