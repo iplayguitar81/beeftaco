2f9f7beda9b4c37b1e57f5fbeb715392ce6b29de
Chris M Snyder <iplayguitar81@yahoo.com>
2016-09-07 22:42:50 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -119,6 +119,19 @@
         <br/>
         <div class="pagination"> {!! $posts->render() !!} </div>
 
+
+
+
+        <h2 id="latest_games" class="Ripper">more news</h2>
+
+        <div class="featurette" id="about">
+            <img class="featurette-image img-circle img-responsive pull-right" src="http://placehold.it/500x500">
+            <h2 class="featurette-heading">This First Heading
+                <span class="text-muted">Will Catch Your Eye</span>
+            </h2>
+            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        </div>
+
         <h2 id="latest_games" class="Ripper">more news</h2>
 
         @foreach($posts as $item)
