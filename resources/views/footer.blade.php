8955f41b7c46cd66a8312f015eab6a3fed3f2b3a
Chris M Snyder <iplayguitar81@yahoo.com>
2016-09-19 22:00:02 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -2,6 +2,6 @@
 <div class="panel-footer">
     Trailblazersfans.com Copyright {{date("Y")}}
     <br/>
-    <a class="text-center" href="{{ route('disclaimer') }}">Disclaimer</a>
+    <a class="text-center" href="{{ route('disclaimer') }}">Site Disclaimer</a>
 </div>
 
