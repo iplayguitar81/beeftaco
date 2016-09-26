956a336ed6637e78107ac32aed95e4f29ad62ce9
Chris M Snyder <iplayguitar81@yahoo.com>
2016-09-25 22:07:57 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -155,7 +155,7 @@
 
                     <p class="second-art-snip">
 
-                        {{$variable}}
+                        {{$variable}}...
 
                         <a class="pull-right" href="{{ route('posts.show', [$item->id, str_slug($item->title)]) }}">READ MORE</a>
 
