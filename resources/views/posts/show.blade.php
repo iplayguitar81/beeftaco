035ccfdd970ba77b8d3ec47d89d5e0c3eed7e22d
Chris M Snyder <iplayguitar81@yahoo.com>
2016-09-26 23:10:23 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -174,29 +174,29 @@
         @endcan
 
         <br/>
-<div class="col-md-12 text-center">
-    <h2 class="text-center Ripper" >leave a disqus comment!</h2>
-<div style="width:85%;" id="disqus_thread"></div>
-    </div>
-<script>
+{{--<div class="col-md-12 text-center">--}}
+    {{--<h2 class="text-center Ripper" >leave a disqus comment!</h2>--}}
+{{--<div style="width:85%;" id="disqus_thread"></div>--}}
+    {{--</div>--}}
+{{--<script>--}}
 
-    /**
-     *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-     *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables */
-    /*
-     var disqus_config = function () {
-     this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
-     this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-     };
-     */
-    (function() { // DON'T EDIT BELOW THIS LINE
-        var d = document, s = d.createElement('script');
-        s.src = '//trailblazersfans-1.disqus.com/embed.js';
-        s.setAttribute('data-timestamp', +new Date());
-        (d.head || d.body).appendChild(s);
-    })();
-</script>
-<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+    {{--/**--}}
+     {{--*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.--}}
+     {{--*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables */--}}
+    {{--/*--}}
+     {{--var disqus_config = function () {--}}
+     {{--this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable--}}
+     {{--this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable--}}
+     {{--};--}}
+     {{--*/--}}
+    {{--(function() { // DON'T EDIT BELOW THIS LINE--}}
+        {{--var d = document, s = d.createElement('script');--}}
+        {{--s.src = '//trailblazersfans-1.disqus.com/embed.js';--}}
+        {{--s.setAttribute('data-timestamp', +new Date());--}}
+        {{--(d.head || d.body).appendChild(s);--}}
+    {{--})();--}}
+{{--</script>--}}
+{{--<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>--}}
 
 
 
