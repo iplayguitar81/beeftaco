574374a0b7b6cffbdf65c71f5277ed26b8f5e978
Chris M Snyder <iplayguitar81@yahoo.com>
2016-09-26 21:08:18 -0700

    get win 10 laravel started yeahhhhhh

--- app/Http/Controllers/boxscoreController.php
+++ app/Http/Controllers/boxscoreController.php
@@ -54,7 +54,7 @@ class boxscoreController extends Controller
         })->export('xls');
 
 
-        return view('boxscores.file_export2', compact('xls'));
+        return view('boxscores.file_export', compact('xls'));
 
     }
 
