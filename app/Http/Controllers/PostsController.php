625694ec40b126052b7ddabd79146ffb657fa848
Chris M Snyder <iplayguitar81@yahoo.com>
2016-09-28 00:19:54 -0700

    get win 10 laravel started yeahhhhhh

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -243,6 +243,12 @@ class PostsController extends Controller
         $post->mainImg_caption = $request->input('mainImg_caption');
         $post->main_article = $request->input('main_article');
         $post->news = $request->input('news');
+        $post->nba = $request->input('nba');
+        $post->retro = $request->input('retro');
+        $post->alum_news = $request->input('alum_news');
+
+
+
 
         $post->body = $request->input('body');
         \Auth::user()->posts()->save($post);
