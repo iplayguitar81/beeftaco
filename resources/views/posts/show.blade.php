ed1426478d4a3ce1fb300be0a0d66af3fb9c5eef
Chris M Snyder <iplayguitar81@yahoo.com>
2016-09-30 17:08:43 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -43,7 +43,7 @@
         <br/>
         <div class="center-block text-center">
        <div class="article-texterson2"> {!! ($post->body) !!} </div>
-            @if(($post->images->id ==$post->id))
+            @if(($post->images->id ==$post->post_id))
             <div class="container">
                 <h2 class='Ripper'>article gallery</h2>
                     <br/>
