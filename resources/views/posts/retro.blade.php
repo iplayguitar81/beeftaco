2f5715fe14ec36100e692e202d1e7e633cc31657
Chris M Snyder <iplayguitar81@yahoo.com>
2016-10-22 15:17:50 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/retro.blade.php
+++ resources/views/posts/retro.blade.php
@@ -15,6 +15,14 @@
 
 
     @foreach($news as $item)
+
+        @if($item->user_id != null)
+            <? $author = App\User::find($item->user_id)->name; ?>
+
+
+        @endif
+
+
     <div class="row">
         <div class="col-sm-4"><a href="#" class=""><img src="../images/md-img-{{ $item->imgPath}}" class="img-responsive"></a>
             <span>{{$item->mainImg_caption}}</span>
@@ -24,7 +32,7 @@
 
             <h3 class="title">{{ $item->title}}</h3>
             <p class="text-muted">{{$item->subHead}}</p>
-            <p class="text-muted">Presented by <a href="#">{{$item->user_id}}</a></p>
+            <p class="text-muted">Written by <a href="#">{{$author}}</a></p>
             <p>{{$item->body}}</p>
 
 
