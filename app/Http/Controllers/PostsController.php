a4d818bf410be0148868e518b69533ac572f284b
Chris M Snyder <iplayguitar81@yahoo.com>
2016-10-21 22:23:44 -0700

    get win 10 laravel started yeahhhhhh

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -528,7 +528,7 @@ class PostsController extends Controller
 
         $news = Post::where('category', '=', $category)->orderBy('created_at', 'desc')->get();
 
-        $news = explode([$news]);
+        $news = explode($news);
         return view('posts.retro', compact('news'));
     }
 
