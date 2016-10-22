9c54c4fd861fd258a7cae6bed1553713395ca123
Chris M Snyder <iplayguitar81@yahoo.com>
2016-10-22 15:00:26 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/retro.blade.php
+++ resources/views/posts/retro.blade.php
@@ -16,7 +16,7 @@
 
     @foreach($news as $item)
     <hr><div class="row">
-        <div class="col-sm-4"><a href="#" class=""><img src="{{$item->imgPath}}" class="img-responsive"></a>
+        <div class="col-sm-4"><a href="#" class=""><img src="../images/md-img-{{ $item->imgPath}}" class="img-responsive"></a>
         </div>
         <div class="col-sm-8">
             <h3 class="title">How to Fight Fraud with Artificial Intelligence and Intelligent Analytics</h3>
