dd5afe8b0e8d827dd9b50bcb838354c7009d1077
Chris M Snyder <iplayguitar81@yahoo.com>
2016-12-20 23:55:47 -0800

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -89,7 +89,7 @@
         <br/>
     </div>
 
-    <div class="col-md-8">
+    <div class="col-md-8 center-block">
 
         <h2 class="text-center Bebas" >leave a facebook comment!</h2>
         <div class="fb-comments center-block" data-href="https://www.trailblazersfans.com/posts/{{$post->id}}/{{str_slug($post->title)}}" data-numposts="10"></div>
