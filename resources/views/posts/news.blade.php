16a31e841939f16e1885a0f02b3ea0b4c064ace4
Chris M Snyder <iplayguitar81@yahoo.com>
2017-05-25 22:40:03 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/news.blade.php
+++ resources/views/posts/news.blade.php
@@ -32,6 +32,8 @@
                 $game_date = $game_date->format('l F dS Y g:i a');
             @endphp
 
+        @endforeach
+
         <div class="col-sm-3">
             <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
             <p>Current Project</p>
