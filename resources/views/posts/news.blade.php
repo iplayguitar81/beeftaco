f0b24e63e6facda0c91186df149d7cbaa1853e66
Chris M Snyder <iplayguitar81@yahoo.com>
2017-05-25 23:45:06 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/news.blade.php
+++ resources/views/posts/news.blade.php
@@ -6,15 +6,14 @@
 <h1 class="article-title-show">latest news categories</h1>
 
 {{--{{$news}}--}}
-<br/>
 
-<a href="{{url('news/general')}}">General News...</a>
-<br/>
-<a href="{{url('news/retro')}}">Retro News...</a>
-<br/>
-<a href="{{url('news/nba')}}">NBA News...</a>
-<br/>
-<a href="{{url('news/former-players')}}">Former Players News...</a>
+{{--<a href="{{url('news/general')}}">General News...</a>--}}
+{{--<br/>--}}
+{{--<a href="{{url('news/retro')}}">Retro News...</a>--}}
+{{--<br/>--}}
+{{--<a href="{{url('news/nba')}}">NBA News...</a>--}}
+{{--<br/>--}}
+{{--<a href="{{url('news/former-players')}}">Former Players News...</a>--}}
 
 
 
