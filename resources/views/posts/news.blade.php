8884840efa7db3dc6971b9d4b997f8efb4d3d548
Chris M Snyder <iplayguitar81@yahoo.com>
2017-05-25 23:10:42 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/news.blade.php
+++ resources/views/posts/news.blade.php
@@ -100,13 +100,6 @@
 
 
 
-
-
-        <div class="col-sm-3">
-            <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
-            <p>Project 2</p>
-        </div>
-
     <hr>
 
 
