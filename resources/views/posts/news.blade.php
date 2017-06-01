846de8818389d409ad71273355b2b8de47a47d0c
Chris M Snyder <iplayguitar81@yahoo.com>
2017-05-31 17:24:27 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/news.blade.php
+++ resources/views/posts/news.blade.php
@@ -31,7 +31,7 @@
             @endphp
 
             <div class="col-sm-3">
-                <h1 class="Ripper"><a class="" href="{{url('news/general')}}">Team News</a></h1>
+                <h1 class="Ripper"><a class="" href="{{url('news/general')}}">team news</a></h1>
                 {{--<img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">--}}
 
                 <img src="../images/md-img-{{ $item->imgPath}}" class="img-responsive" style="width:100%" alt="Team News Image">
@@ -62,7 +62,7 @@
             @endphp
 
         <div class="col-sm-3">
-            <h1 class="Ripper"><a class="" href="{{url('news/retro')}}">Retro News</a></h1>
+            <h1 class="Ripper"><a class="" href="{{url('news/retro')}}">retro news</a></h1>
             <img src="../images/md-img-{{ $item->imgPath}}" class="img-responsive" style="width:100%" alt="Retro News Image">
             <h4 class="secondary-posts-title"><a  href="{{ route('posts.show', [$item->id, str_slug($item->title)]) }}">{{$item->title}}</a><br/>{{$item->date}}</h4>
             @php
