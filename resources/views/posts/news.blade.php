6e7f40fdc7d30dc6c0963520f06883f394005d63
Chris M Snyder <iplayguitar81@yahoo.com>
2017-08-08 23:26:35 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/news.blade.php
+++ resources/views/posts/news.blade.php
@@ -43,6 +43,25 @@
 
 
 
+        <ul class="nav nav-tabs">
+            <li class="active"><a data-toggle="tab" href="#home">past blazers</a></li>
+            <li><a data-toggle="tab" href="#menu1">retro news</a></li>
+        </ul>
+
+        <div class="tab-content">
+            <div id="home" class="tab-pane fade in active">
+                <h3>past blazers</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div id="menu1" class="tab-pane fade">
+                <h3>retro news</h3>
+                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+
+        </div>
+
+
+
 
 
 
