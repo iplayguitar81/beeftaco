fda34f4a34c5620653da8cc2659fb4e2d07ab5bd
Chris M Snyder <iplayguitar81@yahoo.com>
2017-09-19 02:41:20 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -329,8 +329,8 @@
 
 
 @endsection
-<script src="{{url('https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js')}}"></script>
-<script src="{{url('/js/lightslider.js')}}"></script>
+<script src="{{url('https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js')}}" defer></script>
+<script src="{{url('/js/lightslider.js')}}" defer></script>
 <script>
 
     $(document).ready(function() {
